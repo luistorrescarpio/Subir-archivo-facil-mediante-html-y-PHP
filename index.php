106c264584250fcdf957560dcfb200d8c5d926ec
Luis Torres <luitorc@gmail.com>
2019-03-14 06:49:13 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Subir Archivo</title>
+</head>
+<body>
+	<center>
+		<h3>Upload File &lt;form&gt;</h3>
+		<form enctype="multipart/form-data" action="upload_file.php" method="POST">
+			<input name="uploaded_file" type="file" />
+			<input type="submit" value="Subir archivo" />
+		</form>
+		
+	</center>
+</body>
+</html>
